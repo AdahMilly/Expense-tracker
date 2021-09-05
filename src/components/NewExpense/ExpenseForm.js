f5586b605bee5b048fa1005572c474761916d90f
AdahMilly <oamildred@gmail.com>
2021-09-05 18:05:28 +0300

    project setup

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    const titleChangeHandler = () => {}
+    return <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type = "text" onChange={titleChangeHandler}/>
+            </div>
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input type='number' min="0.01" step="0.01" />
+            </div>
+            <div className='new-expense__control'>
+                <label>Date</label>
+                <input type='date' min="2019-10-01" max="2022-12-31" />
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
+            </div>
+        </div>
+    </form>
+}
+
+export default ExpenseForm;
